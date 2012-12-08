2b55c73deed3018af335ed37e9e5ed655e74ba3e
Yoav Ram <yoavram+github@gmail.com>
2012-12-09 01:48:39 +0200

    added favicon, logo, rearranged navbar

--- server.py
+++ server.py
@@ -57,5 +57,5 @@ def index():
 
 if __name__ == '__main__':
 	# Bind to PORT if defined, otherwise default to 5000.
-	port = int(os.environ.get('PORT', 5000))
+	port = int(os.environ.get('PORT', 5001))
 	app.run(host='0.0.0.0', port=port, debug=app.debug)
