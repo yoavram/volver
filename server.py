40c154de3bd84980e755f24fe9260eb7f6d5b53a
Yoav Ram <yoavram+github@gmail.com>
2012-12-10 02:17:33 +0200

    added json result of all posts

--- server.py
+++ server.py
@@ -1,10 +1,26 @@
 # -*- coding: utf-8 -*-
-from flask import Flask, request, render_template, redirect
+from flask import Flask, request, render_template, redirect, Response
 import os
 from flask.ext.pymongo import PyMongo, ASCENDING 
 from datetime import datetime
+from bson import ObjectId
+import simplejson
 
 DATE_FORMAT = "%d/%m/%Y"
+
+class MongoDocumentEncoder(simplejson.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, datetime):
+            return o.isoformat()
+        elif isinstance(o, ObjectId):
+            return str(o)
+        return simplejson.JSONEncoder(self, o)
+
+
+def jsonify(*args, **kwargs):
+    return Response(simplejson.dumps(dict(*args, **kwargs), cls=MongoDocumentEncoder), mimetype='application/json')
+
+
 
 def db_name_from_uri(full_uri):
 	ind = full_uri[::-1].find('/')
@@ -76,7 +92,13 @@ def index():
 		oid = add_post(process_form(request.form.to_dict()))
 		print "Added object to database:", oid
 		return redirect('/')
-	
+
+
+@app.route('/api')
+def posts():
+    posts = get_posts()
+    return jsonify(result=posts)	
+
 
 if __name__ == '__main__':
 	# Bind to PORT if defined, otherwise default to 5000.
