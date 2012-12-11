8f48a4ac41fb3f19f2e5af3816e1149a4d0efa20
Yoav Ram <yoavram+github@gmail.com>
2012-12-11 08:41:33 +0200

    changed ' in bariloche hebrew name

--- atlas.py
+++ atlas.py
@@ -3,7 +3,7 @@
 # http://www.gpsvisualizer.com/geocode
 atlas = {
 	'puerto montt': { 'lat': -41.4717, 'lng': -72.9369, 'he': u'פוארטו מונט', 'en': 'Puerto Montt'}, 
-	'bariloche': {'lat': -41.1334722, 'lng':-71.3102778, 'he': u"בארילוצ'ה", 'en': 'San Carlos de Bariloche'},
+	'bariloche': {'lat': -41.1334722, 'lng':-71.3102778, 'he': u"בארילוצ`ה", 'en': 'San Carlos de Bariloche'},
 	'el bolson': {'lat': -41.9677696, 'lng': -71.5155106, 'he': u'אל בולסון', 'en': 'El Bolsón'},
 	'el chalten': {'lat': -49.3314941, 'lng':-72.886325, 'he': u'אל צ`אלטן', 'en': 'El Chaltén'},
 	'el calafate': {'lat': -50.3402138, 'lng': -72.2705078, 'he': u'אל קלפאטה', 'en': 'El Calafate'},
